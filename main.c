d6a918723742e1f8c3dbd337557caa27c2339ab8
Eric Bruhns <bruhe002@gmail.com>
2023-02-27 16:25:26 -0800

    Got cipher text to print out

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 // Set Up libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 
-
-
+void encrypt(char *pt, int ptLen, char *ct, char *key, char *iv);
+void exceptionHandler(void);
 
 // Set up main function
 int main() {
@@ -20,12 +24,27 @@ int main() {
         exit(-1);
     }
 
-    char word[16];
-    char *c = 0;
+    char word[128];
+    char plaintext[128];
+    char ciphertest[128];
+    char ivStr[128];
 
-    //Get word from word_file
+    unsigned char ciphertext_result[128];
+    int ciphertext_len;
+
+    //Get plaintext string
+    fgets(plaintext, 128, pt_file);
+    printf("%d", (int)strlen(plaintext));
+    // printf("%s\n", plaintext);
+    //Get cipher test text
+    fgets(ciphertest, 128, ct_file);
+
+    // Get IV test
+    fgets(ivStr, 128, iv_file);
+
+    //Get key-word from word_file
     fgets(word, 16, word_file);
-
+    // Pad each word with # if less than 16 characters
     while(word != EOF) {
         for(int i = 0; i < 16; i++){
             if(word[i] == '\n') {
@@ -33,15 +52,56 @@ int main() {
                     word[j] = '#';
                 }
             }
-            printf("%c", word[i]);
+            encrypt(plaintext, strlen(plaintext), ciphertext_result, word, ivStr);
         }
-        printf("\n");
-        fgets(word, 16, word_file);
+        // printf("%s\n", ciphertext_result);
+        fgets(word, 128, word_file);
     }
-
+    
     fclose(pt_file);
     fclose(ct_file);
     fclose(iv_file);
     fclose(word_file);
     return 0;
+}
+
+void exceptionHandler(void) {
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+void encrypt(char *pt, int ptLen, char *ct, char *key, char *iv) {
+    // Create and initialize new cipher context
+    EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+
+    int len;
+
+    int ctLen;
+    
+    // Test if the context exists
+    if(ctx == NULL) {
+        exceptionHandler();
+    }
+
+    // Initialize the encryption operation.
+    if(EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1) {
+        exceptionHandler();
+    }
+
+    // Run the encryption Method
+    if(EVP_EncryptUpdate(ctx, ct, &len, pt, ptLen) != 1) {
+        exceptionHandler();
+    }
+
+    ctLen = len;
+
+    // Finalise the encryption
+    if(EVP_EncryptFinal_ex(ctx, ct + len, &len) != 1) {
+        exceptionHandler();
+    }
+    ctLen += len;
+
+    // Clean up context space
+    EVP_CIPHER_CTX_free(ctx);
 }
