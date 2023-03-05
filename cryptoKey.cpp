e9dcadec8c2a9708af7389da1bab33c4c3b3bb25
Eric Bruhns <bruhe002@gmail.com>
2023-03-05 10:38:55 -0800

    finished encryption program

--- cryptoKey.cpp
+++ cryptoKey.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
@@ -16,49 +17,85 @@ void ascii2Hex(char * in, char* out);
 int main() {
 
     // Upload input files
+    // FILE *pt_file = fopen("file", "r"); //Reading Plaintext
+    // FILE *ct_file = fopen("ciphertext.txt", "r"); //Reading Ciphertext
+    // FILE *iv_file = fopen("iv.txt", "r"); // Reading iv
     FILE *word_file = fopen("words.txt", "r"); // Reading possible keys
 
-    // Initialize Text to be read
-    unsigned char plaintext[] = "This is a top secret.";
+    // FILE *result_ct;
+    //FILE *result_ct_hex = fopen("result_ct.txt", "r");
+
+    unsigned char expectedResult[1024];
+    // fgets(expectedResult, 1024, ct_file);
+    expectedResult[strlen(expectedResult) - 1] = '\0';
+    // // Check if files exist
+    // if (pt_file == NULL || ct_file == NULL || iv_file == NULL) {
+    //     printf("Error! Could not open file\n");
+    //     exit(-1);
+    // }
+
+    unsigned char plaintext[] = "This is top secret.";
     unsigned char ivStr[] =  {170, 187, 204, 221, 238, 255, 0, 153, 136, 119, 102, 85, 68, 51, 34, 17};
     unsigned char ciphertext[] = {0x76, 0x4a, 0xa2, 0x6b, 0x55, 0xa4, 0xda, 0x65, 0x4d, 0xf6, 0xb1, 
                                     0x9e, 0x4b, 0xce, 0x00, 0xf4, 0xed, 0x05, 0xe0, 0x93, 0x46, 0xfb, 
                                     0x0e, 0x76, 0x25, 0x83, 0xcb, 0x7d, 0xa2, 0xac, 0x93, 0xa2}; 
 
+    const char* padding[15] = {"#", "##", "###", "####", "#####", "######", "#######", "########", "#########", "##########",
+                                   "###########", "############", "#############", "##############", "###############"};
 
-    /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    !!!!!FOR SOME REASON THIS LINE OF CODE IS NECESSARY FOR TO GET RESULTS!!!!!
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-    // const char* padding[15] = {"#", "##", "###", "####", "#####", "######", "#######", "########", "#########", "##########",
-    //                                "###########", "############", "#############", "##############", "###############"};
+    // char* key = NULL;
+    // size_t word_length = 0;
+    //     ssize_t number_read;
+
+    //     // Read each word from the file
+    //     while((number_read = getline(&key, &word_length, file)) != -1) {
+    //             // Remove the trailing newline
+    //             key[number_read-1] = '\0';
+
+    //             // Add the padding to the key if necessary
+    //             if((number_read-1) < 16) {
+    //                     int padding_index = 16-number_read;
+    //                     key = strcat(key, padding[padding_index]);
+    //             }
+
+    // fgets(ivStr, 1024, iv_file);
+    // fgets(plaintext, 1024, pt_file);
+    printf("%s\n", ciphertext);
+    printf("%s\n",ivStr);
     unsigned char key[16];
+    size_t word_length = 0;
+    while(getline(&key, &word_length, word_file) != 0) {
+        // for(int i = 0; i <  16; i++) {
+        //     if(word[i] == '\n') {
+        //         for(int j = i; j < 16; j++) {
+        //             word[j] = '#';
+        //         }
+        //         word[16] = '\0';
+        //     };  
+        // }
+                    // Remove the trailing newline
+                key[number_read-1] = '\0';
 
-    // Parse through the words.txt file
-    // Grab the words line by line
-    while(fgets(key, 16, word_file) != NULL) {
-        // Pad the keys with # character
-        for(int i = 0; i <  16; i++) {
-            if(key[i] == '\n') {
-                for(int j = i; j < 16; j++) {
-                    key[j] = '#';
+                // Add the padding to the key if necessary
+                if((number_read-1) < 16) {
+                        int padding_index = 16-number_read;
+                        key = strcat(key, padding[padding_index]);
                 }
-                key[16] = '\0'; // Add terminator character at the end
-            };  
-        } 
 
-        // Make it look "hack-ey"
-        printf("%s    %d      %s\n", key,  strlen(key), ivStr);
+        word[strlen(word) - 1] = '#';    
+        printf("%s    %d\n", word,  strlen(word));
         
-        // Pass the parameters to string, return one if a match is found
-        int answer = encrypt(plaintext, ciphertext, 1, key, ivStr);
+        int answer = encrypt(plaintext, ciphertext, 1, word, ivStr);
         
-        // Display the key
         if(answer == 1) {
-            printf("The key is: %s\n", key);
+            printf("%10dThe key is: %s\n", answer, word);
             break;
         }
     }
     
+    // fclose(pt_file);
+    // fclose(ct_file);
+    // fclose(iv_file);
     fclose(word_file);
     return 0;
 }
@@ -72,26 +109,27 @@ int encrypt(unsigned char *in, unsigned char *out, int do_encrypt, unsigned char
     EVP_CIPHER_CTX *ctx;
     ctx = EVP_CIPHER_CTX_new();
 
+    // Check Key and IV lengths
+    // EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), NULL, NULL, NULL);
+    // OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
+    // OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
+
     // Set the key and IV
     EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
-
-    // Run encryption
+    // printf("AM I IN AN INFINITE LOOP?");
     if(!EVP_EncryptUpdate(ctx, outputBuf, &outputLen, in, strlen(in))) {
         // Error
         EVP_CIPHER_CTX_free(ctx);
         printf("There was an error in update\n");
         return 0;
     }
-
-    // Finalize Blocks
     if(!EVP_EncryptFinal_ex(ctx, outputBuf + outputLen, &tempLen)) {
         //Error
         EVP_CIPHER_CTX_free(ctx);
         printf("There was an error in final\n");
         return 0;
     }
-
-    // Test expected result with actual result
+    // printf("%s   %s\n",outputBuf, out);
     if(memcmp(outputBuf, out, 32) == 0) {
         EVP_CIPHER_CTX_free(ctx);
         printf("THE KEY WORKS!");
